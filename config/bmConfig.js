ff9b3590a233a7825f9ef8474e007048ed83c18b
Gabriel Felipe <56770509+gabrielfelipeio@users.noreply.github.com>
2021-10-15 10:11:57 -0300

    Update bmConfig.js

--- config/bmConfig.js
+++ config/bmConfig.js
@@ -3,4 +3,4 @@ var appEnv = cfenv.getAppEnv();
 var VCAP_LOCAL = require('./vcap');
 var vcapEnv = appEnv.isLocal ? VCAP_LOCAL : JSON.parse(process.env.VCAP_SERVICES);
 
-exports.URL_MONGODB = vcapEnv['compose-for-mongodb'][0].credentials.uri;
+exports.URL_MONGODB = vcapEnv['mongodb-uri'];
